fd508ca82b4990e2eb3a8763d8df91d834f42b0b
Talgin <talgat90.07@gmail.com>
2022-11-08 21:46:14 +0600

    changed prediction function

--- app/main.py
+++ app/main.py
@@ -7,6 +7,8 @@ from fastapi.encoders import jsonable_encoder
 from pydantic import BaseModel
 
 import pandas as pd
+import numpy as np
+from statistics import mode
 from sklearn import preprocessing
 from io import BytesIO
 
@@ -32,6 +34,9 @@ async def get_body(request: Request):
     return {'status': 'hello', 'message': input}
 
 
+def most_common(List):
+    return mode(List)
+
 @app.post("/predictions/driver_identification")
 async def driver_identification(request: Request):
     scaler = preprocessing.MinMaxScaler()
@@ -41,16 +46,59 @@ async def driver_identification(request: Request):
     data = await request.json()
     df = pd.read_json(data)
 
-    result_knn = knn_model.predict(df)
-    result_rf = rf_model.predict(df)
-    result_dt = dt_model.predict(df)
-    print(result_knn, result_dt, result_rf)
-    return data
+    # Prediction classes
+    result_knn_cls = knn_model.predict(df)
+    result_rf_cls = rf_model.predict(df)
+    result_dt_cls = dt_model.predict(df)
+
+    most_common_knn = most_common(result_knn_cls)
+    most_common_rf = most_common(result_rf_cls)
+    most_common_dt = most_common(result_dt_cls)
+    # print(most_common_knn, most_common_rf, most_common_dt)
+    # Prediction with probabilities
+    result_knn = knn_model.predict_proba(df)
+    result_rf = rf_model.predict_proba(df)
+    result_dt = dt_model.predict_proba(df)
+    # print(result_knn, result_dt, result_rf)
+
+    # results_dct = dict()
+    # for i in range(len(result_knn)):
+    #     results_dct['knn_probabilities'] = [np.argmax(), np.max()]
+    
+    knn_flag = False
+    rf_flag = False
+    dt_flag = False
+    # if result_knn >= settings.models_threshold:
+    #     knn_flag = True
+    # if result_rf >= settings.models_threshold:
+    #     rf_flag = True
+    # if result_dt >= settings.models_threshold:
+    #     dt_flag = True
+
+    # If all three predictions are above threshold and point to one ID return ID of driver
+    # if (knn_flag and rf_flag and dt_flag) and (most_common_knn == most_common_rf == most_common_dt):
+    if most_common_knn == most_common_rf == most_common_dt:
+        # print('here')
+        return {"result": True, "message": "Driver found", "values": int(most_common_rf)}
+    else:
+        # Else return each prediction probability and class
+        return {"result": True, "message": "Driver probabilities", "values": [int(most_common_knn), int(most_common_rf), int(most_common_dt)]}
+    
+    # return data
+
     # Normalize incoming stream data
     # df_norm = df(scaler.fit_transform(payload), columns=payload.columns)
     
+    # Get indices of highest probability
+    # for i in range(len(result_knn)):
+    #     np.argmax(result_knn[i]))
+
     # result = loaded_model.predict(df_norm.iloc[0][0])
-    # if result >= 0.75:
+    
+    # If all three predictions are above threshold and point to one ID return ID of driver
+    # if (knn_flag and rf_flag and dt_flag) and :
+
+    # # Else return each prediction probability and class
     #     return {"result": True, "message": "Driver is in database", "score": result}
 
     # return {"result": False, "message": "Driver is NOT in database", "score": result}
@@ -63,8 +111,21 @@ def upload(file: UploadFile = File(...)):
     df = pd.read_csv(buffer)
     buffer.close()
     file.file.close()
-    result_knn = knn_model.predict(df)
-    result_rf = rf_model.predict(df)
-    result_dt = dt_model.predict(df)
+    result_knn_cls = knn_model.predict(df)
+    result_rf_cls = rf_model.predict(df)
+    result_dt_cls = dt_model.predict(df)
+
+    most_common_knn = most_common(result_knn_cls)
+    most_common_rf = most_common(result_rf_cls)
+    most_common_dt = most_common(result_dt_cls)
     # df.to_dict(orient='records')
-    return {'results': {'knn': result_knn.tolist(), 'rf': result_rf.tolist(), 'dt': result_dt.tolist()}}
+    # return {'results': {'knn': result_knn.tolist(), 'rf': result_rf.tolist(), 'dt': result_dt.tolist()}}
+    # If all three predictions are above threshold and point to one ID return ID of driver
+    # if (knn_flag and rf_flag and dt_flag) and (most_common_knn == most_common_rf == most_common_dt):
+    print(most_common_knn, most_common_rf, most_common_dt)
+    if most_common_knn == most_common_rf == most_common_dt:
+        # print('here')
+        return {"result": True, "message": "Driver found", "values": int(most_common_rf)}
+    else:
+        # Else return each prediction probability and class
+        return {"result": True, "message": "Driver probabilities", "values": [int(most_common_knn), int(most_common_rf), int(most_common_dt)]}
